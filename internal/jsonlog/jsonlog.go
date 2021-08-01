d5edc5cdd73ee071cdd35054ba0e05c7dfb1b475
smallX <xieyu559@163.com>
2021-08-02 07:58:41 +0800

    fixed(m): add annotation for const

--- internal/jsonlog/jsonlog.go
+++ internal/jsonlog/jsonlog.go
@@ -15,10 +15,10 @@ type Level int8
 // Initialize constants which represent a specific severity level. We use the iota
 // keyword as a shortcut to assign successive integer values to the constants
 const (
-	LevelInfo Level = iota
-	LevelError
-	LevelFatal
-	LevelOff
+	LevelInfo  Level = iota // Has the value 0.
+	LevelError              // Has the value 1.
+	LevelFatal              // Has the value 2.
+	LevelOff                // Has the value 3.
 )
 
 // Return a human-friendly string for the severity level.
