89dfebef76e54865ea3a2a658a19ef93d1bead40
smallX <xieyu559@163.com>
2021-08-02 10:13:28 +0800

    fix(m): the recoverPanic() middleware wraps our router.

--- cmd/api/routers.go
+++ cmd/api/routers.go
@@ -6,7 +6,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+// Update the routes() method to return a http.Handler instead of a *httprouter.Router.
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	// Convert the notFoundResponse() helper to a http.Handler using the
@@ -29,5 +30,6 @@ func (app *application) routes() *httprouter.Router {
 	// Add the route for the DELETE /v1/movies/:id endpoint.
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
-	return router
+	// Wrap the router with the panic recovery middleware.
+	return app.recoverPanic(router)
 }
