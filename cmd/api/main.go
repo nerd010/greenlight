b495f88c3ddf3fa479caded87f63c89624b7c2b4
smallX <xieyu559@163.com>
2021-08-04 14:26:41 +0800

    feat(c): edit the application struct to contain a new sync.WaitGroup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -63,6 +64,7 @@ type application struct {
 	logger *jsonlog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
