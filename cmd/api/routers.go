5d8cdf5e8acc03ec5c57388119799ac289589063
smallX <xieyu559@163.com>
2021-08-05 23:34:40 +0800

    feat(m): add the route for the POST /v1/tokens/authentication endpoint.

--- cmd/api/routers.go
+++ cmd/api/routers.go
@@ -33,6 +33,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// Add the route for the POST /v1/tokens/authentication endpoint.
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 	// Wrap the router with the panic recovery middleware.
 	// Wrap the router with the rateLimit() middleware.
 	return app.recoverPanic(app.rateLimit(router))
