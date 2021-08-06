df095400884452955cd76ae5d97f5aa75f6a54a5
smallX <xieyu559@163.com>
2021-08-06 14:43:55 +0800

    fix(m): wrap the router with the rateLimit() middleware

--- cmd/api/routers.go
+++ cmd/api/routers.go
@@ -37,5 +37,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 	// Wrap the router with the panic recovery middleware.
 	// Wrap the router with the rateLimit() middleware.
-	return app.recoverPanic(app.rateLimit(router))
+
+	// Use the authenticate() middleware on all requests.
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
