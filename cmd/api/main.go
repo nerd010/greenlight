2e3762e1cb0029028732399b217a976e4aaa3e9d
smallX <xieyu559@163.com>
2021-08-09 16:55:36 +0800

    feat(c): publish the number of active goroutines, database connection pool statistics and current Unix timestamp

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"expvar"
 	"flag"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -151,6 +152,22 @@ func main() {
 	// Publish a new "version" variable in the expvar handler containing our application
 	// version number (currently the constant "1.0.0").
 	expvar.NewString("version").Set(version)
+
+	// Publish the number of active goroutines.
+	expvar.Publish("goroutines", expvar.Func(func() interface{} {
+		return runtime.NumGoroutine()
+	}))
+
+	// Publish the database connection pool statistics.
+	expvar.Publish("database", expvar.Func(func() interface{} {
+		return db.Stats()
+	}))
+
+	// Publish the current Unix timestamp.
+	expvar.Publish("timestamp", expvar.Func(func() interface{} {
+		return time.Now().Unix()
+	}))
+
 	// Declare an instance of the application struct, containing the config struct and
 	// the logger.
 	// Use the data.NewModels() function to initialize a Models struct, passing in the
