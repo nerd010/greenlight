42ae9532dd2ebafdda9de642ebbfa78e975612c5
smallX <xieyu559@163.com>
2021-08-18 18:13:01 +0800

    feat(m): add annotations

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -150,7 +150,7 @@ func (app *application) createPasswordResetTokenHandler(w http.ResponseWriter, r
 }
 
 func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *http.Request) {
-
+	// Parse and validate the user's email address.
 	var input struct {
 		Email string `json:"email"`
 	}
@@ -167,7 +167,8 @@ func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *h
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
-
+	// Try to retrieve the corresponding user record for the email address. If it can't
+	// be found, return an error message to the client.
 	user, err := app.models.Users.GetByEmail(input.Email)
 	if err != nil {
 		switch {
@@ -180,29 +181,36 @@ func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *h
 		return
 	}
 
+	// Return an error if the user has already been activated.
 	if user.Activated {
 		v.AddError("email", "user has already been activated")
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
+	// Otherwise, create a new activation token.
 	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 
+	// Email the user with their additional activation token.
 	app.background(func() {
 		data := map[string]interface{}{
 			"activationToken": token.Plaintext,
 		}
 
+		// Since email addresses MAY be case sensitive, notice that we are sending this
+		// email using the address stored in our database for the user --- not to the
+		// input.Email address provided by the client in this request.
 		err = app.mailer.Send(user.Email, "token_activation.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
 	})
 
+	// Send a 202 Accepted response and confirmation message to the client.
 	env := envelope{"message": "an email will be sent to you containing activation instructions"}
 
 	err = app.writeJSON(w, http.StatusAccepted, env, nil)
